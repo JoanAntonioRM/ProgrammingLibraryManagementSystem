e74b336f685351faf16d015f82359e44844eb861
JoanAntonioRM <joanantonio1207@gmail.com>
2026-05-13 15:39:46 -0400

    Add equals, toString to User and subclasses

--- src/main/java/org/example/domain/Student.java
+++ src/main/java/org/example/domain/Student.java
@@ -1,4 +1,9 @@
 package org.example.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class Student extends User{
 }
--- src/main/java/org/example/domain/Teacher.java
+++ src/main/java/org/example/domain/Teacher.java
@@ -1,4 +1,9 @@
 package org.example.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class Teacher extends User{
 }
