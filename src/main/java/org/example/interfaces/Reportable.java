665224a49708d1995b6df65ecae3e417af7f62ae
JoanAntonioRM <joanantonio1207@gmail.com>
2026-05-13 16:25:21 -0400

    Add and implement methods Reportable interface

--- src/main/java/org/example/interfaces/Reportable.java
+++ src/main/java/org/example/interfaces/Reportable.java
@@ -1,4 +1,24 @@
 package org.example.interfaces;
 
+import org.example.domain.Item;
+import org.example.domain.Library;
+import org.example.domain.User;
+
 public class Reportable {
+
+    static String genItemReport() {
+        StringBuilder report = new StringBuilder();
+        for (Item item : Library.items) {
+            report.append(item).append("\n");
+        }
+        return report.toString();
+    }
+
+    static String genUserReport() {
+        StringBuilder report = new StringBuilder();
+        for (User user : Library.users) {
+            report.append(user).append("\n");
+        }
+        return report.toString();
+    }
 }
