d6183eb66c313811d6db9c08eca617beb1f3b43d
JoanAntonioRM <joanantonio1207@gmail.com>
2026-05-13 15:42:13 -0400

    Add Getter and Setter to User and subclasses

--- src/main/java/org/example/domain/Admin.java
+++ src/main/java/org/example/domain/Admin.java
@@ -1,11 +1,15 @@
 package org.example.domain;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 import org.example.interfaces.Reportable;
 
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
+@Getter
+@Setter
 public class Admin extends User implements Reportable {
 
     public Admin(String name){
--- src/main/java/org/example/domain/Student.java
+++ src/main/java/org/example/domain/Student.java
@@ -1,9 +1,13 @@
 package org.example.domain;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
+@Getter
+@Setter
 public class Student extends User{
 }
