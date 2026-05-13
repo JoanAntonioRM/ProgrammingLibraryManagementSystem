8a27794dd7f5abf37109333932267dd610f42152
JoanAntonioRM <joanantonio1207@gmail.com>
2026-05-13 17:10:57 -0400

    Add custom exception

---
+++ src/main/java/org/example/exceptions/LibraryOperationException.java
@@ -0,0 +1,8 @@
+package org.example.exceptions;
+
+public class LibraryOperationException extends Exception {
+
+    public LibraryOperationException(String message) {
+        super(message);
+    }
+}
