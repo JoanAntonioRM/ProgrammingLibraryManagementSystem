454d5380430dad83d97ce502acf79c943074416e
JoanAntonioRM <joanantonio1207@gmail.com>
2026-05-13 17:52:08 -0400

    Added missing constructor and remove imports not used

--- src/main/java/org/example/domain/Teacher.java
+++ src/main/java/org/example/domain/Teacher.java
@@ -1,18 +1,20 @@
 package org.example.domain;
 
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-public class Teacher extends User{
+public class Teacher extends User {
     private static final int MAX_ITEMS = 10;
 
     public Teacher(String id, String name, List<Item> borrowedItems) {
         super(id, name, borrowedItems);
+    }
+
+    public Teacher(String name) {
+        super(name);
     }
 }
